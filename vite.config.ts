8159f2b3777e053393f576311600721c698a1a21
MakhtarF1 <fallmakh026@gmail.com>
2025-11-04 08:44:38 +0000

    mis a jour de mon portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   server: {
-    host: true, 
-    port: Number(process.env.PORT) || 3000, 
+    host: true,
+    port: Number(process.env.PORT) || 3000,
+
+    // 👇 Ajoute cette ligne pour autoriser Render
+    allowedHosts: ['portfolio-makhtar-fall.onrender.com'],
+  },
+  preview: {
+    allowedHosts: ['portfolio-makhtar-fall.onrender.com'], 
   },
 });
